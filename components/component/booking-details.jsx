9f528b7d386420837158b0c602c4fd390c1c201b
ZZSavagelordZZ <khashab2004@gmail.com>
2024-08-09 01:33:13 +0300

    Added_Pages

--- components/component/booking-details.jsx
+++ components/component/booking-details.jsx
@@ -21,7 +21,7 @@ import { Button } from "@/components/ui/button"
 import { Separator } from "@/components/ui/separator"
 import { Input } from "@/components/ui/input"
 
-export function Booking_details() {
+export function BookingDetails() {
   return (
     (<div className="max-w-4xl mx-auto py-8 px-4 sm:px-6 lg:px-8">
       <div className="grid gap-8">
